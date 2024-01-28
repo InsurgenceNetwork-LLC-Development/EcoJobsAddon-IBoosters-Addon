06dc1e232fcf5b26aa912ecbe125217b00b0aedd
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 19:30:29 -0500

    api jar update

--- src/main/java/com/insurgencedev/ecojobsaddon/EcoJobsAddon.java
+++ src/main/java/com/insurgencedev/ecojobsaddon/EcoJobsAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.ecojobsaddon;
 
 import com.insurgencedev.ecojobsaddon.listeners.EcoJobsEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EcoJobsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoJobs Support")
 public class EcoJobsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("EcoJobs")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("EcoJobs")) {
             registerEvent(new EcoJobsEventListener());
         }
     }
